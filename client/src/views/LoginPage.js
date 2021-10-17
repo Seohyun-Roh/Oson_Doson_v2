26dab100c116715d12ee37a024af1c4b9190ceca
Seohyun-Roh <radult951@gmail.com>
2021-10-17 15:44:59 +0900

    로그인 기능 구현

--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react'
+import axios, { post } from 'axios';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router';
 
-export default class LoginPage extends Component {
-    render() {
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItem: 'center',
+        width: '100%',
+        margin: '30px 30px 30px 30px'
+    }
+});
+
+class LoginPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userid: '',
+            userpw: ''
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        // this.props.history.push('/');
+        console.log(this.state);
+        axios.post('/api/users/login', 
+        { userid: this.state.userid, userpw: this.state.userpw})
+        .then((response) => {
+            console.log(response.data)
+            if(response.data.success){
+                this.props.history.push('/')
+            } else{
+                alert(response.data.message);
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
+    render(){
+        const { classes } = this.props;
         return (
-            <div>
-                로그인 페이지
+            <div className={classes.root}>
+                <TextField label="ID" type="text" name="userid" value={this.state.userid} onChange={this.handleValueChange} /><br/>
+                <TextField label="Password" type="password" name="userpw" value={this.state.userpw} onChange={this.handleValueChange} /><br/>
+                <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Login</Button>
             </div>
         )
     }
 }
+
+export default withRouter(withStyles(styles)(LoginPage));
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.json())
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
@@ -27,7 +27,8 @@ const multer = require('multer')
 // 서버의 루트 폴더에 있는 upload 폴더를 사용자의 파일이 업로드가 되는 공간으로 설정
 const upload = multer({dest: './upload'}) 
 
-app.get('/api/users',(req,res) => {
+// 회원 정보 출력(SELECT)
+app.get('/api/users', (req,res) => {
     connection.query(
       "SELECT * FROM USER WHERE isDeleted = 0",
       (err, rows, fields) => {
@@ -36,6 +37,7 @@ app.get('/api/users',(req,res) => {
     )
 })
 
+// 회원가입
 // upload 폴더를 사용자가 접근해서 프로필이미지를 확인할 수 있도록 함
 app.use('/image', express.static('./upload')) // image 폴더에서 해당 폴더에 접근할 수 있도록 함
 app.post('/api/users', upload.single('image'), (req, res) => {
@@ -46,7 +48,7 @@ app.post('/api/users', upload.single('image'), (req, res) => {
   let userid = req.body.userid;
   let userpw = req.body.userpw;
   let params = [image, name, birth, userid, userpw];
-  
+
   bcrypt.hash(params[4], saltRounds, (err, hash) => {
     params[4] = hash
     connection.query(sql, params,
@@ -58,6 +60,7 @@ app.post('/api/users', upload.single('image'), (req, res) => {
   })
 })
 
+// 회원 삭제 (isDeleted->1로 업데이트)
 app.delete('/api/users/:id', (req, res) => {
   let sql = 'UPDATE USER SET isDeleted = 1 WHERE id = ?';
   let params = [req.params.id];
@@ -66,6 +69,42 @@ app.delete('/api/users/:id', (req, res) => {
       res.send(rows);
     }
   )
+})
+
+// 로그인
+app.post('/api/users/login', (req, res) => {
+  let userid = req.body.userid;
+  let userpw = req.body.userpw;
+  let sql = 'SELECT * FROM USER WHERE userid = ?';
+  let params = [userid, userpw];
+
+  connection.query(sql, params[0], (err, row) => {
+    if(err) console.log(err)
+    
+    if(row.length>0){ //ID가 존재하면
+      bcrypt.compare(params[1], row[0].userpw, (err, result) => {
+        if(result){
+          res.cookie("loginUser", userid).status(200).json({
+            success: true,
+            userid: userid
+          })
+          console.log("성공")
+        } else {
+          res.json({
+            success: false,
+            message: '비밀번호가 틀렸습니다.'
+          })
+          console.log("비밀번호 틀림")
+        }
+      })
+    } else{
+      res.json({
+        success: false,
+        message: '존재하지 않는 id입니다.'
+      })
+      console.log("id없음")
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
