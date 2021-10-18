48e1b33476712ee63e216999b55a2a2220524145
Seohyun-Roh <radult951@gmail.com>
2021-10-18 23:48:48 +0900

    모듈 정리

--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios, { post } from 'axios';
+import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,16 +1,6 @@
 import React, {Component} from 'react';
 import '../App.css';
 import { withStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button';
-import Drawer from '@material-ui/core/Drawer';
-import MenuItem from '@material-ui/core/MenuItem';
 
 import MenuBar from '../components/MenuBar';
 
@@ -23,12 +13,7 @@ const styles = theme => ({
 
 class Main extends Component{
 
-  constructor(props){
-    super(props);
-  }
-
   render(){ 
-    const {classes}=this.props;
     return (
       <MenuBar />
     )
