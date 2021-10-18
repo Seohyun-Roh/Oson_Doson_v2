5463a8ef0fd5c39402d981594d72bbcc92340de9
Seohyun-Roh <radult951@gmail.com>
2021-10-18 22:26:43 +0900

    Add Menubar

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,24 +1,34 @@
 import React, {Component} from 'react';
-import User from '../components/User';
 import '../App.css';
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
+import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
 import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 
 const styles = theme => ({
   root:{
     width:'100%',
     overflowX:"auto"
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   }
 })
 
@@ -27,11 +37,56 @@ class Main extends Component{
   constructor(props){
     super(props);
     this.state = {
+      toggle: false
     }
   }
 
-  render(){ 
+  handleButtonClick = (e) => {
+    this.props.history.push('/login')
+  }
 
+  handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+
+  render(){ 
+    const {classes}=this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+              오손도손🐾
+            </Typography>
+            <div className={classes.grow} />
+            <Button color="inherit" onClick={this.handleButtonClick}>Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer open={this.state.toggle}>
+          <MenuItem onClick={this.handleDrawerToggle}>
+            <Link component={RouterLink} to="/">
+            Home
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={this.handleDrawerToggle}>
+            <Link component={RouterLink} to="/">
+            진료예약
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={this.handleDrawerToggle}>
+            <Link component={RouterLink} to="/">
+            예약조회
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={this.handleDrawerToggle}>
+            <Link component={RouterLink} to="/">
+            진료내역조회
+            </Link>
+          </MenuItem>
+        </Drawer>
+      </div>
+    )
   }
 }
 
