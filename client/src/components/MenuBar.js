720e4bbdfd01fe62c054101ff06294d9bca561c7
Seohyun-Roh <radult951@gmail.com>
2021-10-18 23:15:10 +0900

    Create MenuBar Component

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import '../App.css';
+import { withStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = theme => ({
+    root:{
+      width:'100%',
+      overflowX:"auto"
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    drawer:{
+      position: 'relative'
+    },
+    btnClose:{
+      position: 'absolute',
+      right: '4px'
+    }
+  })
+
+class MenuBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          toggle: false
+        }
+    }
+    
+    handleButtonClick = (e) => {
+        this.props.history.push('/login')
+    }
+    
+    handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+
+    render() {
+        const {classes}=this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                    오손도손🐾
+                    </Typography>
+                    <div className={classes.grow} />
+                    <Button color="inherit" onClick={this.handleButtonClick}>Login</Button>
+                </Toolbar>
+                </AppBar>
+                <Drawer open={this.state.toggle} className={classes.drawer}>
+                <MenuItem onClick={this.handleDrawerToggle}>
+                    <Link component={RouterLink} to="/">
+                    Home
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={this.handleDrawerToggle} className={classes.btnClose}>
+                    X
+                </MenuItem>
+                <MenuItem onClick={this.handleDrawerToggle}>
+                    <Link component={RouterLink} to="/">
+                    진료예약
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={this.handleDrawerToggle}>
+                    <Link component={RouterLink} to="/">
+                    예약조회
+                    </Link>
+                </MenuItem>
+                <MenuItem onClick={this.handleDrawerToggle}>
+                    <Link component={RouterLink} to="/">
+                    진료내역조회
+                    </Link>
+                </MenuItem>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(MenuBar);
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -12,30 +12,12 @@ import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
 import MenuItem from '@material-ui/core/MenuItem';
 
+import MenuBar from '../components/MenuBar';
+
 const styles = theme => ({
   root:{
     width:'100%',
     overflowX:"auto"
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  drawer:{
-    position: 'relative'
-  },
-  btnClose:{
-    position: 'absolute',
-    right: '4px'
   }
 })
 
@@ -43,59 +25,12 @@ class Main extends Component{
 
   constructor(props){
     super(props);
-    this.state = {
-      toggle: false
-    }
   }
-
-  handleButtonClick = (e) => {
-    this.props.history.push('/login')
-  }
-
-  handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
 
   render(){ 
     const {classes}=this.props;
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              오손도손🐾
-            </Typography>
-            <div className={classes.grow} />
-            <Button color="inherit" onClick={this.handleButtonClick}>Login</Button>
-          </Toolbar>
-        </AppBar>
-        <Drawer open={this.state.toggle} className={classes.drawer}>
-          <MenuItem onClick={this.handleDrawerToggle}>
-            <Link component={RouterLink} to="/">
-            Home
-            </Link>
-          </MenuItem>
-          <MenuItem onClick={this.handleDrawerToggle} className={classes.btnClose}>
-              X
-          </MenuItem>
-          <MenuItem onClick={this.handleDrawerToggle}>
-            <Link component={RouterLink} to="/">
-            진료예약
-            </Link>
-          </MenuItem>
-          <MenuItem onClick={this.handleDrawerToggle}>
-            <Link component={RouterLink} to="/">
-            예약조회
-            </Link>
-          </MenuItem>
-          <MenuItem onClick={this.handleDrawerToggle}>
-            <Link component={RouterLink} to="/">
-            진료내역조회
-            </Link>
-          </MenuItem>
-        </Drawer>
-      </div>
+      <MenuBar />
     )
   }
 }
