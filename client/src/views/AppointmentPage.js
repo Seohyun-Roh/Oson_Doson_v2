de9d95de1094102c043f53fd3f1ee55b12f45308
Seohyun-Roh <radult951@gmail.com>
2021-10-19 23:25:27 +0900

    날짜 선택 & 시간 선택 추가
    "@date-io/date-fns": "^1.3.13",
    "@date-io/dayjs": "^1.3.13",
    "@date-io/moment": "^1.3.9",
    "@material-ui/pickers": "^3.2.10",
    "draft-js": "^0.11.7",
    "moment": "^2.26.0"
    버전 맞추기

--- client/src/views/AppointmentPage.js
+++ client/src/views/AppointmentPage.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react'
 import MenuBar from '../components/MenuBar';
+import { DatePicker, MuiPickersUtilsProvider, TimePicker } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
 
 class AppointmentPage extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedDate: null,
+            selectedTime: null,
+            checkedB: false
+        };
+    }
+
+    handleDateChange = (date) => {
+        this.setState({ selectedDate: date });
+        console.log(this.state.selectedDate)
+    }
+
+    handleTimeChange = (time) => {
+        this.setState({ selectedTime: time });
+        console.log(this.state.selectedDate)
+    }
+
     render() {
+        const { classes } = this.props;
         return (
             <div>
                 <MenuBar />
-                진료 예약 페이지
+                진료 내역 확인 페이지
+                <div>
+                    <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <TimePicker
+                        keyboard="true" placeholder="24 hours"
+                        clearable
+                        ampm={false}
+                        label="24 hours"
+                        value={this.state.selectedTime}
+                        onChange={this.handleTimeChange}
+                    />
+                    <DatePicker
+                        keyboard="true" placeholder="MM/DD/YYYY" format={"MM/DD/YYYY"}
+                        value={this.state.selectedDate}
+                        mask={[/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/]}
+                        onChange={this.handleDateChange}
+                        disableopenonenter="true"
+                        animateYearScrolling={false}
+                        autoOk={true}
+                        clearable
+                        />
+                    <p>{JSON.stringify(this.state.selectedDate)}</p>
+                    <p>{JSON.stringify(this.state.selectedTime)}</p>
+                    </MuiPickersUtilsProvider>
+                </div>
             </div>
         )
     }
