49608b1de57de3bd96dacd18884ff84766449b88
Seohyun-Roh <radult951@gmail.com>
2021-11-04 00:43:06 +0900

    Modified RegisterPage CSS

--- client/src/views/RegisterPage.js
+++ client/src/views/RegisterPage.js
@@ -3,6 +3,9 @@ import { post } from 'axios';
 import { TextField, Button, withStyles } from '@material-ui/core';
 import { withRouter } from 'react-router';
 
+import puppy from '../img/puppy.jpg';
+import MenuBar from '../components/MenuBar';
+
 const styles = theme => ({
     root: {
         display: 'flex',
@@ -16,6 +19,70 @@ const styles = theme => ({
     },
     Input: {
         marginTop: 100
+    },
+    loginForm: {
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginRight:'40px',
+        paddingRight:'10px'
+    },
+    pageContainer:{
+        display:'grid',
+        placeItems:'center',
+        height:'83vh',
+        backgroundColor:'#eff0f2'
+    },
+    loginFormBox:{
+        width:'85%',
+        maxWidth:'900px',
+        height:'75%',
+        maxHeight:'800px',
+        display:'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        gridColumnGap: '20px',
+        boxShadow:'27px 43px 43px -26px rgb(89 89 89 / 39%)',
+        backgroundColor:'#ffffff',
+        borderRadius:'6px'
+    },
+    logo:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginLeft:'20px',
+        '& img':{
+            width:'30vw',
+            height:'40vw',
+            maxWidth:'370px',
+            maxHeight:'400px',
+        }
+    },
+    btnRegister:{
+        fontSize:'15px',
+        textAlign:'center',
+        textDecoration:'none',
+        fontFamily: 'Noto Sans KR',
+        height:'30px',
+        display:'inline-block',
+        border:'none',
+        borderRadius:'4px',
+        backgroundColor:'#d9d9d9',
+        padding:'3px 0 3px 0',
+        '&:hover':{
+            cursor:'pointer',
+            backgroundColor:'#ffc000'
+        },
+        transition:'0.3s ease'
+    },
+    register:{
+        display:'flex',
+        justifyContent:'flex-end',
+        alignItems:'center',
+        marginTop:'10px',
+        '& span':{
+            fontSize:'14px',
+            color:'#666565'
+        }
     }
 });
 
@@ -85,20 +152,48 @@ class Register extends React.Component {
     render(){
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <h1>Register</h1>
-                <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
-                <label htmlFor="raised-button-file">
-                    <Button variant="contained" color="primary" component="span" name="file">
-                        {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
-                    </Button>
-                </label>
-                <br/>
-                <TextField label="ID" type="text" name="userid" value={this.state.userid} onChange={this.handleValueChange} /><br/>
-                <TextField label="Password" type="password" name="userpw" value={this.state.userpw} onChange={this.handleValueChange} /><br/>
-                <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
-                <TextField label="생년월일" type="text" name="birth" value={this.state.birth} onChange={this.handleValueChange} /><br/>
-                <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+            <div>
+                <MenuBar />
+                <div className={classes.pageContainer}>
+                    <div className={classes.loginFormBox}>
+                        <div className={classes.logo}>
+                            <img src={puppy} alt="puppy"/>
+                        </div>
+                        <div className={classes.loginForm}>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+                            <label htmlFor="raised-button-file">
+                                <Button variant="contained" component="span" name="file"
+                                style={{
+                                    borderRadius: '4px',
+                                    backgroundColor: '#ffc000',
+                                    fontSize: '14px',
+                                    marginBottom: '5px'
+                                }}>
+                                    {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                                </Button>
+                            </label>
+                            <TextField label="ID" type="text" name="userid" value={this.state.userid} onChange={this.handleValueChange} /><br/>
+                            <TextField label="Password" type="password" name="userpw" value={this.state.userpw} onChange={this.handleValueChange} /><br/>
+                            <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                            <TextField label="생년월일" type="text" name="birth" value={this.state.birth} onChange={this.handleValueChange} /><br/>
+                            <button className={classes.btnRegister} onClick={this.handleFormSubmit}>회원가입</button>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className={classes.root}>
+                    <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+                    <label htmlFor="raised-button-file">
+                        <Button variant="contained" color="primary" component="span" name="file">
+                            {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                        </Button>
+                    </label>
+                    <br/>
+                    <TextField label="ID" type="text" name="userid" value={this.state.userid} onChange={this.handleValueChange} /><br/>
+                    <TextField label="Password" type="password" name="userpw" value={this.state.userpw} onChange={this.handleValueChange} /><br/>
+                    <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                    <TextField label="생년월일" type="text" name="birth" value={this.state.birth} onChange={this.handleValueChange} /><br/>
+                    <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                </div> */}
             </div>
         )
     }
