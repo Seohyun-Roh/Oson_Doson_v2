d2ae746d4b69e8df30f0db6bbaee3df057cd6127
Seohyun-Roh <radult951@gmail.com>
2021-11-15 10:00:00 +0900

    registerChoice 추가

--- client/src/views/RegisterChoicePage.js
+++ client/src/views/RegisterChoicePage.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 
 export default class RegisterChoicePage extends Component {
+
+    handleButtonClick = (e) => {
+        e.preventDefault();
+        this.props.history.push('/register');
+    }
+
     render() {
         return (
             <div>
-                <Button variant="contained" color="primary">일반 회원 회원 가입</Button>
+                <Button variant="contained" color="primary" onClick={this.handleButtonClick}>일반 회원 회원 가입</Button>
                 <Button variant="contained" color="primary">병원 회원 회원 가입</Button>
             </div>
         )
