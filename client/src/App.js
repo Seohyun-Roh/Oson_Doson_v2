b77574295afb15c3539e790685d8f95f6eb3b17a
Seohyun Roh <radult951@gmail.com>
2022-01-06 23:42:15 +0900

    Update App.js
    코드 순서 변경(메인, 유저, 병원)

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Main from "./views/Main";
-
 import LoginPage from "./views/LoginPage";
 import RegisterChoicePage from "./views/RegisterChoicePage";
-import UserRegisterPage from "./views/UserRegisterPage";
-import HospitalRegisterPage from "./views/HospitalRegisterPage";
 
+import UserRegisterPage from "./views/UserRegisterPage";
+import UserMyPage from "./views/UserMyPage";
 import AppointmentPage from "./views/AppointmentPage";
 import AppointmentCheckPage from "./views/AppointmentCheckPage";
-
 import MedHistoryPage from "./views/MedHistoryPage";
 
+import HospitalRegisterPage from "./views/HospitalRegisterPage";
 import UserManagePage from "./views/UserManagePage";
 
 function App() {
@@ -23,17 +22,18 @@ function App() {
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register_choice" component={RegisterChoicePage} />
+
+          <Route exact path="/user_mypage" component={UserMyPage} />
           <Route exact path="/user_register" component={UserRegisterPage} />
+          <Route exact path="/appt" component={AppointmentPage} />
+          <Route exact path="/appt_check" component={AppointmentCheckPage} />
+          <Route exact path="/med_history" component={MedHistoryPage} />
+
           <Route
             exact
             path="/hospital_register"
             component={HospitalRegisterPage}
           />
-
-          <Route exact path="/appt" component={AppointmentPage} />
-          <Route exact path="/appt_check" component={AppointmentCheckPage} />
-          <Route exact path="/med_history" component={MedHistoryPage} />
-
           <Route exact path="/usermanage" component={UserManagePage} />
         </Switch>
       </div>
